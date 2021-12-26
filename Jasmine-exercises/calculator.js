081214ce3b4fd61c15f9a11db1e8ee03f179fd43
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2021-12-26 15:04:25 -0800

    wrote tests, but failed

--- Jasmine-exercises/calculator.js
+++ Jasmine-exercises/calculator.js
@@ -1,14 +1,28 @@
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
+  const paymentUI = document.getElementById("monthly-payment");
+
   if (form) {
     setupIntialValues();
     form.addEventListener("submit", function(e) {
       e.preventDefault();
+      paymentUI.innerText = '';
       update();
       form.reset();
     });
   }
 });
+
+function setupIntialValues() {
+  const values  = { amount: 10000, years: 10, rate: 4.5 };
+  const amountUI = document.getElementById("loan-amount");
+  amountUI.value = values.amount;
+  const yearsUI = document.getElementById("loan-years");
+  yearsUI.value = values.years;
+  const rateUI = document.getElementById("loan-rate");
+  rateUI.value = values.rate;
+  update();
+}
 
 function getCurrentUIValues() {
   return {
@@ -18,23 +32,14 @@ function getCurrentUIValues() {
   }
 }
 
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
-function setupIntialValues() {
-}
 
-// Get the current values from the UI
-// Update the monthly payment
 function update() {
   const span = document.querySelector('span');
   span.append(calculateMonthlyPayment());
 }
 
-// Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
-function calculateMonthlyPayment() {
+
+function calculateMonthlyPayment(values) {
   let loanAmt = document.getElementById("loan-amount").value
   let loanYrs = document.getElementById("loan-years").value
   let loanRate = document.getElementById("loan-rate").value
@@ -43,16 +48,8 @@ function calculateMonthlyPayment() {
   let numberOfPayments = loanYrs*12;
   let monthlyPayment = ((loanAmt*periodicInterestRate)/(1 - Math.pow(1 + periodicInterestRate, -numberOfPayments))).toFixed(2);
   
-
-  return "$" + monthlyPayment;
+  return monthlyPayment;
 }
 
 
 
-
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
-function updateMonthly(monthly) {
-  
-  
-}
