25618390135cfc25439a127c4b2b015cedf3b191
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2021-12-26 13:11:33 -0800

    created calculation

--- Jasmine-exercises/calculator.js
+++ Jasmine-exercises/calculator.js
@@ -26,15 +26,31 @@ function setupIntialValues() {
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const span = document.querySelector('span');
+  // span.append(calculateMonthlyPayment());
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
+function calculateMonthlyPayment() {
+  let loanAmt = document.getElementById("loan-amount").value
+  let loanYrs = document.getElementById("loan-years").value
+  let loanRate = document.getElementById("loan-rate").value
+  
+  let periodicInterestRate = (loanRate/12)*.01;
+  let numberOfPayments = loanYrs*12;
+  let monthlyPayment = ((loanAmt*periodicInterestRate)/(1 - Math.pow(1 + periodicInterestRate, -numberOfPayments))).toFixed(2);
+  
+  return monthlyPayment;
 }
+
+
+
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  
+  
 }
